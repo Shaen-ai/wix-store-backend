dd3907ea5aa7d0d59827d82bdd3cf9861c8907f8
Shahen <shahen1@Shahens-MacBook-Pro.local>
2026-03-12 13:24:21 +0400

    asd

--- config/cors.php
+++ config/cors.php
@@ -2,25 +2,18 @@
 
 return [
     'paths' => ['api/*'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => array_filter([
-        env('FRONTEND_WIDGET_URL'),
-        env('FRONTEND_DASHBOARD_URL'),
-        env('FRONTEND_SETTINGS_URL'),
-        'http://localhost:5174',
-        'http://localhost:5175',
-        'http://localhost:5176',
-    ]),
-    'allowed_origins_patterns' => [
-        '#^https://.*\.wix\.com$#',
-        '#^https://.*\.wixsite\.com$#',
-        '#^https://.*\.wixstatic\.com$#',
-        '#^https://.*\.wix-code\.com$#',
-        '#^https://.*\.editorx\.com$#',
-        '#^https://.*\.editor\.wix\.com$#',
-    ],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => false,
 ];
